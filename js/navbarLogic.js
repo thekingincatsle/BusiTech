71e973ea73ea65b43bbf74eeb4992d56b57ccc7a
Duy Nguyễn <duykhanhchi1993@gmail.com>
2023-03-12 17:52:14 +0700

    add organizers section and partners section

--- js/navbarLogic.js
+++ js/navbarLogic.js
@@ -1,22 +1,27 @@
 const bgNavChangeOnscroll = () => {
-    const navbar = document.getElementsByClassName('navbar').item(0);
-    const navHeight = navbar.offsetHeight;
-    window.onscroll = function () {
-        if (document.body.scrollTop > navHeight || document.documentElement.scrollTop >= navHeight) {
-            navbar.classList.add('bg-primary')
-        } else navbar.classList.remove('bg-primary')
-    }
-}
+  const navbar = document.getElementsByClassName("navbar").item(0);
+  const navHeight = navbar.offsetHeight;
+  window.onscroll = function () {
+    if (
+      document.body.scrollTop > navHeight ||
+      document.documentElement.scrollTop >= navHeight
+    ) {
+      navbar.classList.add("bg-primary");
+    } else navbar.classList.remove("bg-primary");
+  };
+};
 
 const implementDropdownMenu = () => {
-    const btn = document.getElementsByClassName('dropdownBtn').item(0);
-    const dropdownContent = document.getElementsByClassName('dropdownContent').item(0);
-    btn.addEventListener('click', () => {
-        dropdownContent.classList.toggle('active')
-    })
-}
+  const btn = document.getElementsByClassName("dropdownBtn").item(0);
+  const dropdownContent = document
+    .getElementsByClassName("dropdownContent")
+    .item(0);
+  btn.addEventListener("click", () => {
+    dropdownContent.classList.toggle("active");
+  });
+};
 
 setTimeout(() => {
-    bgNavChangeOnscroll();
-    implementDropdownMenu();
+  bgNavChangeOnscroll();
+  implementDropdownMenu();
 }, 100);
