91196dc87a8f0eeafefecee1efba0508dd5fe6b8
Duy Nguyễn <duykhanhchi1993@gmail.com>
2023-03-13 08:20:35 +0700

    fix form response

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ window.addEventListener("load", function (event) {
   // your code ....
   const form = document.getElementById("form");
   const submitButton = document.getElementById("submit");
-  console.log(form, submitButton);
   const scriptURL =
-    "https://script.google.com/macros/s/AKfycbwuRe2wy6YjZ9zc1r4ozwIFrRhybULVIyjUpxPp1afhsv_7bMLzucM34pBXu6Ex4eY1/exec";
+    "https://script.google.com/macros/s/AKfycbzxlG0MkZOAeEcZctc3FjI8pFzLpPGV5t5UffpQ6oXX_p35Fo5qp2HiLAa2pF97Cdr-Kw/exec";
 
   form.addEventListener("submit", (e) => {
     submitButton.disabled = true;
     e.preventDefault();
+    console.log(form);
     let requestBody = new FormData(form);
+    console.log(requestBody);
+    // requestBody.source = requestBody.source.toString();
     fetch(scriptURL, { method: "POST", body: requestBody })
       .then((response) => {
         alert("Success!", response);
