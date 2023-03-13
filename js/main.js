da9af73f4c75168f8590c63d230ede62c7084ebb
Duy Nguyễn <duykhanhchi1993@gmail.com>
2023-03-13 12:29:40 +0700

    update form behavior after submit

--- js/main.js
+++ js/main.js
@@ -2,19 +2,28 @@ window.addEventListener("load", function (event) {
   // your code ....
   const form = document.getElementById("form");
   const submitButton = document.getElementById("submit");
+  const loadingIcon = document.getElementById("loading-icon");
   const scriptURL =
     "https://script.google.com/macros/s/AKfycbzxlG0MkZOAeEcZctc3FjI8pFzLpPGV5t5UffpQ6oXX_p35Fo5qp2HiLAa2pF97Cdr-Kw/exec";
 
   form.addEventListener("submit", (e) => {
     submitButton.disabled = true;
     e.preventDefault();
-    console.log(form);
+    const inputs = document.querySelectorAll(".input-field");
+    const checkbox = document.querySelectorAll('input[type="checkbox"]');
     let requestBody = new FormData(form);
-    console.log(requestBody);
-    // requestBody.source = requestBody.source.toString();
+
+    // clear input value after submit
+    inputs.forEach((input) => {
+      input.value = "";
+    });
+    checkbox.forEach((i) => (i.checked = false));
+
+    loadingIcon.classList.remove("hidden");
     fetch(scriptURL, { method: "POST", body: requestBody })
       .then((response) => {
-        alert("Success!", response);
+        loadingIcon.classList.add("hidden");
+        alert("Your application form is sent successfully!", response);
         submitButton.disabled = false;
       })
       .catch((error) => {
