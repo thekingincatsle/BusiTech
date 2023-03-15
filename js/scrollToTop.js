aaadea8a78bb4341f14f29b2db299905dc596444
thekingincatsle <dat.dt200133@sis.hust.edu.vn>
2023-03-15 22:38:46 +0700

    final commit

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,39 +1,6 @@
-window.onload = function () {
-  let myButton;
+var myButton = document.getElementById("btn-back-to-top");
 
-  myButton = document.getElementById("btn-back-to-top");
-
-  window.onscroll = function () {
-    scrollFunction();
-  };
-
-  window.scroll({
-    top: 2500,
-    left: 0,
-    behavior: "smooth",
-  });
-
-  window.scrollBy({
-    top: 100,
-    left: 0,
-    behavior: "smooth",
-  });
-
-  function scrollFunction() {
-    if (
-      document.body.scrollTop > document.body.clientHeight / 2 ||
-      document.documentElement.scrollTop > document.body.clientHeight / 2
-    ) {
-      myButton.style.display = "block";
-    } else {
-      myButton.style.display = "none";
-    }
-  }
-
-  myButton.addEventListener("click", backToTop);
-
-  function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
-};
+myButton.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
